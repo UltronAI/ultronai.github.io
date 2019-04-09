83e3d0f7f09b2e67be9c49033672446b8d7efc12
Feng Gao <gaof.student@gmail.com>
2019-04-09 11:08:54 +0800

    Site updated: 2019-04-09 11:08:50

--- resume/update.sh
+++ resume/update.sh
@@ -1,8 +1,9 @@
 source activate py2
 jemdoc index.jemdoc
 
-git add .
-git commit -m "update my resume"
-git push
-
+#git add .
+#git commit -m "update my resume"
+#git push
+cd ../..
+hexo g && hexo d
 
